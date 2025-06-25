d9fb338459380aaa0cfda08c82e3f8b820634d39
kal576 <cmuuo5864@gmail.com>
2025-06-25 08:47:42 +0300

    Add alx_book_store.sql

---
+++ alx_book_store.sql
@@ -0,0 +1,38 @@
+#Database for an online bookstore. It contains books, authors, customers, orders and order details.
+
+CREATE TABLE Books(
+	book_id INT PRIMARY KEY,
+	title VARCHAR(130),
+	author_id INT,
+	price DOUBLE,
+	publication_date DATE,
+	FOREIGN KEY(author_id) REFERENCES Authors(author_id)
+);
+
+CREATE TABLE authors(
+	author_id INT PRIMARY KEY,
+	author_name VARCHAR(215)
+);
+
+CREATE TABLE customers(
+	customer_id INT PRIMARY KEY,
+	customer_name VARCHAR(215),
+	email VARCHAR(215),
+	address TEXT
+);
+
+CREATE TABLE order_details(
+        order_id INT PRIMARY KEY,
+        customer_id INT,
+	FORIGEIGN KEY (customer_id) REFERENCES customer(customer_id),
+);
+
+CREATE TABLE order_details(
+	orderdetailid INT PRIMARY KEY,
+	order_id INT,
+	book_id INT,
+	quantity DOUBLE,
+	FOREIGN KEY (order_id) REFERENCES orders(order_id),
+        FOREIGN KEY (book_id) REFERENCES books(book_id),
+
+);
