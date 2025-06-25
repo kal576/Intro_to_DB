ec5a93d7ed9baccf4688c9822eb9c25244465b8c
kal576 <cmuuo5864@gmail.com>
2025-06-25 09:04:36 +0300

    Add order_date to orders table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -30,12 +30,12 @@ CREATE TABLE Orders(
 	FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE order_details(
+CREATE TABLE Order_Details(
 	orderdetailid INT PRIMARY KEY,
 	order_id INT,
 	book_id INT,
 	quantity DOUBLE,
-	FOREIGN KEY (order_id) REFERENCES orders(order_id),
-        FOREIGN KEY (book_id) REFERENCES books(book_id),
+	FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+        FOREIGN KEY (book_id) REFERENCES Books(book_id),
 
 );
